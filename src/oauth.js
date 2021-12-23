dcb493ba9e9b3f625de4793b44a4c15f38f0652e
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-12-23 19:04:39 +0800

    Update oauth.js

--- src/oauth.js
+++ src/oauth.js
@@ -53,7 +53,16 @@ module.exports.verify = (config, settings) => {
        if(req.isAuthenticated()) {
         const data = await settings.findOne({});
          if(data?.logViewers?.includes(req.user.id)) return next();
-         return res.send(`You can't view the logs`);
+         const content = `<div class="message-group hide-overflow">
+         <div class="avatar-large" style="background-image: url(https://cdn.discordapp.com/embed/avatars/0.png)"></div>
+         <div class="comment">
+             <div class="message" style="height: 25px;">
+                <strong class="username">Modmail</strong> <span class="timestamp">${new Date()}</span>
+             </div>
+            <div style="background-color:#49443c;border-left:3px solid #faa81a;padding:5px;">You can't view the logs, you may request access from admins.</div>
+         </div>
+     </div>`
+         return res.render("log", { content, auth: config?.oAuth2 == true ? true : false });
        }
       return res.redirect(`/login?redirect=${req.params.id}`);
    }
